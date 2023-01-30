dd7cc043330ffb5de3e5ee2035e550bb3179db45
Jan Fooken <git@bode.fun>
2023-01-30 08:44:27 +0100

    Update .shell/utils.sh

--- dot_shell/utils.sh
+++ dot_shell/utils.sh
@@ -116,7 +116,7 @@ fix_ssh_permissions() {
     if [ -d "$HOME/.ssh" ]; then
         chmod 700 "$HOME/.ssh"
         chmod 600 "$HOME/.ssh"/*
-        
+
         if [ "$(\find $HOME/.ssh/ -type f \( -name "*.pub" \) | wc -l)" -gt 0 ]; then
             chmod 644 "$HOME/.ssh"/*.pub
         fi
@@ -124,7 +124,7 @@ fix_ssh_permissions() {
         if [ -f "$HOME/.ssh/authorized_keys" ]; then
             chmod 644 "$HOME/.ssh/authorized_keys"
         fi
-        
+
         if [ -f "$HOME/.ssh/allowed_signers" ]; then
             chmod 644 "$HOME/.ssh/allowed_signers"
         fi
