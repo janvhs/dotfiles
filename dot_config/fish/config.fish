4cbb5ce0f06e3c74fe2dc2adf5937aa5d06f3fa3
Jan Fooken <git@bode.fun>
2023-04-01 09:33:28 +0200

    Remove .config/fish/conf.d/00-brew.fish Remove .config/fish/conf.d/01-darwin.fish Remove .config/fish/conf.d/01-wsl.fish Remove .config/fish/conf.d/02-common.fish Remove .config/fish/conf.d/ls_on_cd.fish Remove .config/fish/conf.d/magic-enter.fish Remove .config/fish/config.fish Remove .config/fish/.gitignore Remove .config/fish/functions/bode_fix_ssh_permissions.fish Remove .config/fish/functions/bode_is_installed.fish Remove .config/fish/functions/bode_is_ssh.fish Remove .config/fish/functions/bode_os_discovery.fish Remove .config/fish/functions/bode_suggest_all_installed.fish Remove .config/fish/functions/bode_suggest_installed.fish Remove .config/fish/functions/fish_user_key_bindings.fish Remove .config/fish/functions/movie_sleep.fish Remove .config/fish/functions/refresh_brewfile.fish Remove .config/fish/functions/wslenv.fish

--- dot_config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-# ./conf.d gets loaded before this file,
-# so I store my config there.
-
-# The os-dependent part of the configuation is stored
-# in a file with their respective names, while the
-# os independent part is stored in common.fish.
-
-# TODO: Maybe port oh-my-zsh's macos plugin to fish or use https://github.com/mattmc3/fishconf/tree/51e43c3887af4e216513938de2bdad2710fa53cd/plugins/macos/functions 
--- dot_config/fish/functions/bode_is_ssh.fish
+++
@@ -1,8 +0,0 @@
-# TODO: Test this function
-function bode_is_ssh --description "Check if the current shell is a SSH shell"
-    if test -n "$SSH_CLIENT" -o -n "$SSH_TTY" -o -n "$SSH_CONNECTION"
-        return 0
-    else
-        return 1
-    end
-end
